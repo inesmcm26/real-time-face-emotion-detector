ceff608481174a3e738cf3bb81a75a4f87274360
inesmcm <ines.magessi@tecnico.ulisboa.pt>
2023-03-04 19:05:00 +0000

    vgg architecture added

--- src/model_definition.py
+++ src/model_definition.py
@@ -1,7 +1,7 @@
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, ZeroPadding2D
 from keras.models import Sequential
 
-def VGG(input_shape = (48, 48, 1)):
+def VGG(input_shape = (48, 48, 1), n_emotions = 7):
 
     model = Sequential()
     model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=input_shape))
@@ -32,7 +32,7 @@ def VGG(input_shape = (48, 48, 1)):
     model.add(Dense(4096, activation='relu'))
     model.add(BatchNormalization())
     model.add(Dropout(0.3))
-    model.add(Dense(7, activation='softmax'))
+    model.add(Dense(n_emotions, activation='softmax'))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
